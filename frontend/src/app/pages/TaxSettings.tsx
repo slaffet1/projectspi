6dd63ed23069d2efcc53c4f0ef772edb3611a616
slaffet1 <mohamedsaid.leffat@esprit.tn>
2026-05-05 19:03:05 +0100

    url invoice settings et invoice settings fix1

--- frontend/src/app/pages/TaxSettings.tsx
+++ frontend/src/app/pages/TaxSettings.tsx
@@ -6,9 +6,7 @@
   import { Pencil, Trash2, Plus, X, Settings } from "lucide-react";
   import { useBusiness } from "@/app/context/BusinessContext";
 
-  const API_URL = import.meta.env.PROD 
-  ? "http://charikty.swedencentral.cloudapp.azure.com:3001" 
-  : "/api";
+  const API_URL = "/api";
   
 
   const AdminTaxSettings = ({ businessId }: { businessId: number | undefined }) => {
