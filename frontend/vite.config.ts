5ddec11b685b09ca2c629f5aee35776ac9a01dfc
slaffet1 <mohamedsaid.leffat@esprit.tn>
2026-05-05 10:05:53 +0100

    change in vite url

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -15,6 +15,17 @@ export default defineConfig({
     },
     dedupe: ['react', 'react-dom'],
   },
+  server: {
+    host: true, // Needed for your CI's --host 0.0.0.0 flag
+    port: 5173,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3001',
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
   assetsInclude: ['**/*.svg', '**/*.csv'],
   build: {
     rollupOptions: {
